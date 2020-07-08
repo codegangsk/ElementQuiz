cc596117cc5031147324e729a83fd4016c058f89
Sophie Kim <kjhj0604@gmail.com>
2020-07-08 07:22:50 -0700

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Sophie Kim on 2020/07/08.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
